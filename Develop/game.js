eee956888d897bb63972f23f2f734b430ddfa483
Chase Rivera <achaserivera@gmail.com>
2021-05-29 17:51:20 -0700

    added starting psuedo code for html and JS

---
+++ Develop/game.js
@@ -0,0 +1,7 @@
+// DECLARE a starting 'score'
+
+// DECLARE an array list of 'questions'
+    // Where each value is an object to define a question and its properties
+
+// store a reference to the element for the 'timerDisplayEL'
+//store a ref to the 'startButtonEl'
