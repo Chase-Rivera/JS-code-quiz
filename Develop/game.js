db351ad565ea5df6bdce5e121d44bb30300af8d5
Chase Rivera <achaserivera@gmail.com>
2021-05-29 18:22:21 -0700

    added to JS psuedo code for starting steps

--- Develop/game.js
+++ Develop/game.js
@@ -3,5 +3,36 @@
 // DECLARE an array list of 'questions'
     // Where each value is an object to define a question and its properties
 
+// DECLARE a 'questionIndex' to point to the current question
+
 // store a reference to the element for the 'timerDisplayEL'
 //store a ref to the 'startButtonEl'
+
+
+// create 'startGame' function
+    
+    // Call 'startTimer'
+    //Hide the start screen element and show the first question
+
+
+// Create 'endGame' function
+
+// Cerate 'startTimer' function
+    
+    // initializing the string 'timeLeft' value
+    //start the running countdown on the timer
+
+// Create 'answerQuestion' function
+
+    // get the value associated with the clicked 'button'
+    // Compare against the corect answer for the current question
+
+        // If correct ...
+        // If incorrect ..
+            // Subtract points from 'score'
+
+        // Continue to the next question 'renderCurrentQuestion()'
+
+// Creat 'renderCurrentQuestion' function
+
+    // Access the current question datta from question[questionIndex]
