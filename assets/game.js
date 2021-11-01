8dbc1a41993f75f1df0101bc86dfe14b374babe0
Chase Rivera <achaserivera@gmail.com>
2021-11-01 15:35:05 -0700

    added highscore function to track high scores to local storage and to move the player to the highscores page after quiz ends

--- assets/game.js
+++ assets/game.js
@@ -91,122 +91,20 @@ function complete(){
     finalScoreEl.textContent=timerLeft;
 };
 
+function highScore(){
+    endPageEl.style.display="none";
+    var highScores=JSON.parse(localStorage.getItem("highscores"))|| [];
+    var newScore={
+        initials: plyrInitEl.value,
+        score: timerLeft
+    };
+    highScores.push(newScore);
+    localStorage.setItem("highscores", JSON.stringify(highScores));
+    window.location.replace("./highScores.html");
+};
 
-
-function startGame() {
-    
-    startButton.classList.add('hide')
-    
-    currentQuestionindex = questions
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
-    startTimer();
-    timerLeft = 75
-}
-
-function setNextQuestion() {
-    showQuestion([currentQuestionIndex])
-
-}
+startBtnEl.onclick=startGame
+submitBtnEl.onclick=highScore
 
 
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-
-}
-
-function selectAnswer() {
-
-}
-
-var questions =  [
-    {
-       question: 'who is the leader of the Autobots',
-       answers: [
-           { text: 'Optimus Prime', correct: true},
-           { text: 'Megatron', correct: false},
-           { text: 'Bumblebee', correct: false},
-           { text: 'Starscream', correct: false}
-       ]
-    
-    }
-]
-
-
-
-function startTimer() {
-    var timerLeft = 75
-    timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        
-        if (isWin && timerCount > 0) {
-          
-          clearInterval(timer);
-          winGame();
-        }
-      }
-      
-      if (timerCount === 0) {
-        
-        clearInterval(timer);
-        loseGame();
-      }
-    }, 1000);
-  }
-
-  startButton.addEventListener('click', startGame)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DECLARE a starting 'score'
-
-// DECLARE an array list of 'questions'
-    // Where each value is an object to define a question and its properties
-
-// DECLARE a 'questionIndex' to point to the current question
-
-// store a reference to the element for the 'timerDisplayEL'
-//store a ref to the 'startButtonEl'
-
-
-// create 'startGame' function
-    
-    // Call 'startTimer'
-    //Hide the start screen element and show the first question
-
-
-// Create 'endGame' function
-
-// Cerate 'startTimer' function
-    
-    // initializing the string 'timeLeft' value
-    //start the running countdown on the timer
-
-// Create 'answerQuestion' function
-
-    // get the value associated with the clicked 'button'
-    // Compare against the corect answer for the current question
-
-        // If correct ...
-        // If incorrect ..
-            // Subtract points from 'score'
-
-        // Continue to the next question 'renderCurrentQuestion()'
-
-// Creat 'renderCurrentQuestion' function
-
-    // Access the current question datta from question index array*/
